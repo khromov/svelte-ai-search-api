eacf707dda860e266f3855b10bffb9595ac8e618
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2026-02-22 23:40:48 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,32 @@
+import { generateText } from "ai";
+import { experimental_createMCPClient as createMCPClient } from "ai/mcp";
+import { gateway } from "ai";
+
 const server = Bun.serve({
   routes: {
     "/_health": (req) => {
       return new Response("OK");
+    },
+    "/q": async (req) => {
+      const question = "What is $state in Svelte?";
+      const mcpClient = await createMCPClient({
+        transport: {
+          type: "http",
+          url: "https://mcp.svelte.dev/mcp",
+        },
+      });
+
+        const model = gateway.languageModel("anthropic/claude-sonnet-4-5-20250929");
+
+        const result = await generateText({
+            model,
+            tools: await mcpClient.tools(),
+            maxSteps: 10,
+            prompt: question,
+        });
+
+
+        return new Response("OK");
     },
   },
   // fallback for unmatched routes:
