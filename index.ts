107523ba6a8198e253ce98ab83bfa10167ecc6b4
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2026-02-22 21:29:39 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  routes: {
+    "/_health": new Response("OK"),
+  },
+  // fallback for unmatched routes:
+  fetch(req) {
+    return new Response("Not Found", { status: 404 });
+  },
+});
+
+console.log(`Server running at ${server.url}`);
